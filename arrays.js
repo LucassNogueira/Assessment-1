80b88847c60445a88362a59515753589baae1571
LucassNogueira <simplylucasjason@gmail.com>
2022-01-07 10:14:52 -0600

    problem 3 and 4 done

--- arrays.js
+++ arrays.js
@@ -1,30 +1,31 @@
 //////////////////PROBLEM 1////////////////////
 /* Create a copy of the faveColors array called 'colorCopy' using the slice method. */
 
-const faveColors = ['red', 'green', 'black']
+// const faveColors = ['red', 'green', 'black']
 
-//CODE HERE
+// //CODE HERE
 
-let colorCopy=faveColors.slice(0);
-console.log(colorCopy);
+// let colorCopy=faveColors.slice(0);
+// console.log(colorCopy);
 
 
 //////////////////PROBLEM 2////////////////////
 /* Add a fourth color to the end of the 'colorCopy' array using the push method. */
 
 //CODE HERE
-colorCopy.push('blue');
-console.log(colorCopy);
+// colorCopy.push('blue');
+// console.log(colorCopy);
 //////////////////PROBLEM 3////////////////////
 /*
   Using an array method, create a new array called 'middleNums' from the 
   numbers array that will capture only the middle numbers (2, 3, 4).
 */
 
-const numbers = [1, 2, 3, 4, 5]
+// const numbers = [1, 2, 3, 4, 5]
 
-//CODE HERE
-
+// //CODE HERE
+// let middleNums = numbers.splice(1,3);
+// console.log(middleNums);
 
 //////////////////PROBLEM 4////////////////////
 /* 
@@ -39,3 +40,12 @@ const numbers = [1, 2, 3, 4, 5]
 const bigOrSmallArray = [1, 99, 42, 69, 102, -10, 159, 352]
 
 // CODE HERE
+let answers = [];
+
+for (let i=0;i < bigOrSmallArray.length; i++){
+  if(bigOrSmallArray[i] <= 100){
+    answers.push('small');
+  } else {
+    answers.push('big');
+  }
+} console.log(answers);
