ea125eb278e80e5cce04874cc25be73ed4ed0f74
LucassNogueira <simplylucasjason@gmail.com>
2022-01-07 11:25:24 -0600

    switched the conditions so that now it allows password to be 10char

--- password.js
+++ password.js
@@ -8,10 +8,10 @@ const reader = readline.createInterface({
 console.log('Welcome to the password validation tool!');
 
         reader.question("Please validate your password!", function(answer) {
-        if (answer.length< 10) {
-          console.log(`That password is only ${answer.length} charaters long, please try again.`);
+        if (answer.length >= 10) {
+          console.log(`That password long enough, great!`);
         } else {
-          console.log(`That password is long enough, great!`);
+          console.log(`TThat password is only ${answer.length} charaters long, please try again.`);
         }
       
         reader.close();
