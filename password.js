03e4bfae8b2d1116f5e755e742717e71bf37b753
LucassNogueira <simplylucasjason@gmail.com>
2022-01-07 11:12:03 -0600

    got the must be 10 charaters

--- password.js
+++ password.js
@@ -1,1 +1,17 @@
-console.log('test');
+const readline = require("readline");
+
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+console.log('Welcome to the password validation tool!');
+
+        reader.question("Please validate your password!", function(answer) {
+        if (answer.length< 10) {
+          console.log("Password is not long enough.");
+        } else {
+          console.log("That password is long enough!");
+        }
+      });
+
