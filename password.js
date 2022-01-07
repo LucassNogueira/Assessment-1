2531edfae445149ea81ab38908503c2a4af3ace0
LucassNogueira <simplylucasjason@gmail.com>
2022-01-07 11:20:50 -0600

    got it working, but i want to make it loop

--- password.js
+++ password.js
@@ -9,9 +9,14 @@ console.log('Welcome to the password validation tool!');
 
         reader.question("Please validate your password!", function(answer) {
         if (answer.length< 10) {
-          console.log("Password is not long enough.");
+          console.log(`That password is only ${answer.length} charaters long, please try again.`);
         } else {
-          console.log("That password is long enough!");
+          console.log(`That password is long enough, great!`);
         }
-      });
-
+      
+        reader.close();
+    
+    
+    });
+    
+  
