b30eaf7dc6abe9186d92931c03febe1ff3c13a26
LucassNogueira <simplylucasjason@gmail.com>
2022-01-07 09:50:55 -0600

    SDLC done done

--- sldc.js
+++ sldc.js
@@ -20,3 +20,12 @@
 */
 const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
 
+const analysisOfRequirements = 'In this stage, developers will identify what problem or task they are trying to solve. What types of information/technologies will be required to complete said task/problem.'
+
+const design = 'In the design stage the developers assigned to the project will begin to literally design how the process will work/look. Not necessarily graphic deisgn, but layout of what goes where and how users will interact with objects.'
+
+const implimentation = 'The creation of the code / skeleton of the code, beigning to see the project come to life and able to run it. Basically putting action to the first 3 steps and actually writing code.'
+
+const testingIntegration = 'This would be as the title states, testing the code for bugs and integrating this new function or feature to the main code.'
+
+const maintain = 'In this last step you are continually checking what you are adding is not breaking previous code and creating functionability issues with prior steps of the certain product/project. The SDLC is basically an endless loop in a sense becuase developers constantly rotate thru steps in order to ensure smooth opperating code.'
